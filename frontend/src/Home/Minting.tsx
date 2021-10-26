9f37e6c0d2b0a15847021197c91cc995d7516e4d
Stanislas Bernard <bernardstanislas@gmail.com>
2021-10-26 22:51:32 +0200

    :lipstick: Desktop responsiveness

--- frontend/src/Home/Minting.tsx
+++ frontend/src/Home/Minting.tsx
@@ -5,13 +5,16 @@ export const Minting = () => {
   return (
     <div className="bg-pink-400 p-6">
       <Title>Start your egg collection!</Title>
-      <div className="flex justify-center">
-        <img className="w-2/3 shadow-md" src="1.gif" alt="first-animation" />
-      </div>
-      <div className="flex justify-center py-6">
-        <button className="bg-white p-4 border border-pink-800 shadow-md font-display text-xl text-pink-400 rounded-md">
-          <strong>Presale on 01/11/21</strong>
-        </button>
+      <div className="flex flex-col md:flex-row-reverse items-center md:py-16">
+        <div className="md:flex-1 shadow-m flex justify-center items:center">
+          <img className="md:w-2/3 " src="1.gif" alt="first-animation" />
+        </div>
+
+        <div className="flex justify-center py-6 md:flex-1">
+          <button className="bg-white p-4 border border-pink-800 shadow-md font-display text-xl text-pink-400 rounded-md">
+            <strong>Presale on 01/11/21</strong>
+          </button>
+        </div>
       </div>
     </div>
   );
