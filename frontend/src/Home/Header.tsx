ae212bfc5c73e6dc0c9a7ed5e331129d8a977ad4
Stanislas Bernard <bernardstanislas@gmail.com>
2021-10-26 22:36:09 +0200

    :lipstick: Compress images and put desktop background

--- frontend/src/Home/Header.tsx
+++ frontend/src/Home/Header.tsx
@@ -3,18 +3,15 @@ import React from "react";
 export const Header = () => {
   return (
     <>
-      <div
-        className="bg-fixed bg-contain p-6"
-        style={{ backgroundImage: `url("banner1-mobile.png")` }}
-      >
-        <div className="backdrop-brightness-90 backdrop-blur leading-10 text-3xl font-display drop-shadow p-4 my-8 rounded">
+      <div className="bg-fixed bg-contain p-6 md:p-12 bg-banner-mobile md:bg-banner-desktop bg-cover flex flex-col items-center">
+        <div className="backdrop-brightness-90 backdrop-blur leading-10 text-3xl font-display drop-shadow p-4 my-8 rounded md:w-96">
           <h1>Welcome to the</h1>
           <h1>
             <strong>GangstaEggs Factory</strong>
           </h1>
         </div>
         <div className="flex justify-center">
-          <div className="border p-4 bg-white text-black rounded drop-shadow-lg">
+          <div className="border p-4 bg-white text-black rounded drop-shadow-lg md:m-6">
             <p>🥚 9,999 randomly generated NFT eggs on Ethereum blockchain</p>
             <p>🐣 Evolve your eggs into cute chicks</p>
             <p>💞 Breed your chicks to create new eggs!</p>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         'display': ['Saira', 'ui-sans-serif', 'system-ui'],
         'body': ['Courier', 'ui-sans-serif', 'system-ui'],
+      },
+      extend: {
+        backgroundImage: {
+          'banner-mobile': 'url("banner1-mobile.jpg")',
+          'banner-desktop': 'url("banner1.jpg")',
+         }
       }
     },
     variants: {},
