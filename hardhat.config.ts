0a7767a8763cf4b936154a705cc761a0c7c6c1d1
Stanislas Bernard <bernardstanislas@gmail.com>
2021-11-02 00:03:31 +0100

    :sparkles: Shared wallet contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,7 +28,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.6",
   react: {
     providerPriority: ["web3modal", "hardhat"],
   },
