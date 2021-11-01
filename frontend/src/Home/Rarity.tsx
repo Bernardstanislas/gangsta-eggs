4b1d03824aac3df19433c7de452f2ddbb76bc9ee
Stanislas Bernard <bernardstanislas@gmail.com>
2021-11-01 15:58:58 +0100

    :sparkles: Egg assets for rarity

--- frontend/src/Home/Rarity.tsx
+++ frontend/src/Home/Rarity.tsx
@@ -6,50 +6,108 @@ import { Subtitle } from "../components/Subtitle";
 export const Rarity = () => {
   return (
     <Section title="Rarity" withBorder={true}>
-      <Subtitle>Types</Subtitle>
+      <Subtitle>Eyes</Subtitle>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f8f780230b44c70bd4133_type-normal.png"
-          title="Normal"
-          percentage={80}
+          imageUrl="/images/rarity/eyes/green.png"
+          title="Green"
+          percentage={45}
         />
         <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9bcc7e9d9d4b26487e25_unnamed-2.png"
-          title="Alien"
-          percentage={5}
-        />
-        <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9bf922465159efef83ef_unnamed-3.png"
-          title="Zombie"
-          percentage={5}
-        />
-        <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9c4083a703b689f4718d_unnamed-4.png"
-          title="Spotted"
+          imageUrl="/images/rarity/eyes/blue_glasses.png"
+          title="Blue glasses"
           percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/eyes/green_glasses.png"
+          title="Green glasses"
+          percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/eyes/black_glasses.png"
+          title="Black glasses"
+          percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/eyes/eyebrow_glasses.png"
+          title="Eyebrow glasses"
+          percentage={7}
+        />
+        <Card
+          imageUrl="/images/rarity/eyes/orange_glasses.png"
+          title="Orange glasses"
+          percentage={5}
         />
       </div>
-      <Subtitle>Types</Subtitle>
+      <Subtitle>Facial hair</Subtitle>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f8f780230b44c70bd4133_type-normal.png"
-          title="Normal"
-          percentage={80}
+          imageUrl="/images/rarity/facial_hair/none.png"
+          title="None"
+          percentage={65}
         />
         <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9bcc7e9d9d4b26487e25_unnamed-2.png"
-          title="Alien"
-          percentage={5}
-        />
-        <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9bf922465159efef83ef_unnamed-3.png"
-          title="Zombie"
-          percentage={5}
-        />
-        <Card
-          imageUrl="https://uploads-ssl.webflow.com/612f754973590a388c869510/612f9c4083a703b689f4718d_unnamed-4.png"
-          title="Spotted"
+          imageUrl="/images/rarity/facial_hair/angry_eyebrows.png"
+          title="Angry eyebrows"
           percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/facial_hair/eyebrows.png"
+          title="Eyebrows"
+          percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/facial_hair/texan_beard.png"
+          title="Texan beard"
+          percentage={7}
+        />
+        <Card
+          imageUrl="/images/rarity/facial_hair/grey_beard.png"
+          title="Grey beard"
+          percentage={5}
+        />
+        <Card
+          imageUrl="/images/rarity/facial_hair/black_beard.png"
+          title="Black beard"
+          percentage={3}
+        />
+      </div>
+      <Subtitle>Hats</Subtitle>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <Card
+          imageUrl="/images/rarity/hats/none.png"
+          title="None"
+          percentage={30}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/helmet.png"
+          title="Helmet"
+          percentage={18}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/beanie.png"
+          title="Beanie"
+          percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/cap.png"
+          title="Cap"
+          percentage={10}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/headband.png"
+          title="Headband"
+          percentage={8}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/cowboy.png"
+          title="Cowboy"
+          percentage={6}
+        />
+        <Card
+          imageUrl="/images/rarity/hats/clown.png"
+          title="Clown"
+          percentage={6}
         />
       </div>
     </Section>
