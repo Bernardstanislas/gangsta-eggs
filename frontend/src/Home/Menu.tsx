cfabadf56a6296dd30c5b38c4b598634a217fc60
Stanislas Bernard <bernardstanislas@gmail.com>
2021-11-01 17:53:58 +0100

    :lipstick: Logo

--- frontend/src/Home/Menu.tsx
+++ frontend/src/Home/Menu.tsx
@@ -6,7 +6,7 @@ export const Menu = () => {
     <>
       <div className="border-b h-16 text-black fixed w-full bg-pourpre z-10 flex items-center">
         <div className="sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg flex items-center justify-between mx-auto flex-1 px-4">
-          <FontAwesomeIcon className="text-white" icon="egg" size="2x" />
+          <img src="/logo.png" alt="logo" className="h-12 drop-shadow"></img>
           <div className="text-white space-x-4">
             <a href="https://twitter.com/GangstaEggs">
               <FontAwesomeIcon icon={["fab", "twitter"]} size="2x" />
