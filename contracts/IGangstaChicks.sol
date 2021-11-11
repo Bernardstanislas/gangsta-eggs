29768e303fa56d7fb5d6a6016fa7581d667a5701
Stanislas Bernard <bernardstanislas@gmail.com>
2021-11-11 11:38:15 +0100

    :fire: Remove unused contracts

--- contracts/IGangstaChicks.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-
-interface IGangstaChicks {
-    function mintChickFromEgg(address _to, string calldata _ipfsCid) external returns (bool);
-}
--- contracts/SharedWallet.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract SharedWallet is PaymentSplitter {
-    constructor(address[] memory payees, uint256[] memory shares_) PaymentSplitter(payees, shares_) payable {}
-}
