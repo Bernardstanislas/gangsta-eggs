f866936bac861ada931b10251d296ab24571311e
Stanislas Bernard <bernardstanislas@gmail.com>
2021-11-26 19:11:10 +0100

    :wrench: Configure SSL

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,5 +1,9 @@
-import {Pool} from 'pg';
+import { Pool } from "pg";
 
 export const postgresPool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
