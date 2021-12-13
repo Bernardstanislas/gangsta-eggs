834985a6a6e4a6319ad5a1bec395d2aaa1d68228
Stanislas Bernard <bernardstanislas@gmail.com>
2021-12-14 00:26:26 +0100

    :bug: Comment missing file

--- tasks/index.ts
+++ tasks/index.ts
@@ -4,6 +4,6 @@ export * from "./deploy-proxy";
 export * from "./upgrade-proxy";
 export * from "./deploy-forwarder";
 export * from "./sign-minting-meta-tx";
-export * from "./relay-meta-tx";
+// export * from "./relay-meta-tx";
 export * from "./set-trusted-forwarder";
 export * from "./owner-of";
