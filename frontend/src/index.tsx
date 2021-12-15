cd374ccf64b304da773c40faace7718e3849c710
Stanislas Bernard <bernardstanislas@gmail.com>
2021-12-15 11:43:16 +0100

    :sparkles: Add Sentry

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,9 +6,21 @@ import App from "./App";
 import * as FullStory from "@fullstory/browser";
 import { Symfoni } from "./hardhat/SymfoniContext";
 import { ToastContainer } from "react-toastify";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "react-toastify/dist/ReactToastify.css";
 
 FullStory.init({ orgId: "169KHR" });
+
+Sentry.init({
+  dsn: "https://f1ee8f0aab974509abcb8e265e79ec2c@o1091325.ingest.sentry.io/6108111",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <BrowserRouter>
